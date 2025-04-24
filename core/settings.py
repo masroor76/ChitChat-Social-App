3d69d89cc114b08e4da117e8f6ee409cefec5af5
Masroor Amir <masrooramir76@gmail.com>
2025-04-24 17:17:02 +0500

    Added Style, connected PostgreSQL Database

--- core/settings.py
+++ core/settings.py
@@ -77,12 +77,26 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'chitchat',
+        'USER': 'masroor',
+        'PASSWORD': '461705',
+        'HOST': 'localhost',  
+        'PORT': '5432',        
     }
 }
+
+
 
 
 # Password validation
